67ad9242d5813088522a17c141bfd9702dbea1bc
KarpovDmitrii <n_eco@mail.ru>
2026-04-24 15:11:51 +0600

    feat: setup database connection with kysely and postgres

---
+++ src/database/database.module.ts
@@ -0,0 +1,40 @@
+import { Global, Module, Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { Pool } from 'pg';
+import { Kysely, PostgresDialect, sql } from 'kysely';
+
+@Global()
+@Module({
+  providers: [
+    {
+      provide: 'DB',
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        const logger = new Logger('Database');
+
+        const dialect = new PostgresDialect({
+          pool: new Pool({
+            host: configService.get<string>('DB_HOST'),
+            port: configService.get<number>('DB_PORT'),
+            user: configService.get<string>('DB_USERNAME'),
+            password: configService.get<string>('DB_PASSWORD'),
+            database: configService.get<string>('DB_DATABASE'),
+          }),
+        });
+
+        const db = new Kysely<any>({ dialect });
+
+        try {
+          await sql`SELECT 1`.execute(db);
+          logger.log('Successfully сonnection to database');
+        } catch {
+          logger.error('Failed to connect to database');
+        }
+
+        return db;
+      },
+    },
+  ],
+  exports: ['DB'],
+})
+export class DatabaseModule {}
